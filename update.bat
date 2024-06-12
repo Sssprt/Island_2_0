99f2d49fa6f5b6c14baaf4b3112d99a2bac53429
DiZoloto <m8945807@gmail.com>
2024-06-12 09:07:33 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,3 +1,4 @@
 @echo off
+del /Q /S C:\Users\%USERNAME%\AppData\Roaming\.minecraft\versions\ostrov2_0\mods\*.*
 xcopy %CD% C:\Users\%USERNAME%\AppData\Roaming\.minecraft\versions\ostrov2_0 /S /Y
 pause
